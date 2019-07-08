03346777d619daa8ea3eae177852ef5db9512b45
Dandhi12 <50889795+Dandhi12@users.noreply.github.com>
2019-07-09 06:45:42 +0700

    divisa hotel
    the fix divisa hotel

---
+++ fungtion.js
@@ -0,0 +1,26 @@
+
+
+                 
+                //  print fungtion
+
+                  function printDiv(print) {
+                    var printContents = document.getElementById(print).innerHTML;
+                    w=window.open();
+                    w.document.write(printContents);
+                    w.print();
+                    w.close();
+                }
+
+                  
+                  
+
+        
+                
+                 
+                  
+                  
+                 
+
+      
+                  
+                  
